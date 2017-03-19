5c9b8ae000fb9c725f00d95d77ebf764b7139b6e
austin <austinogilvie@gmail.com>
2017-03-19 14:44:20 -0400

    all r6 leaderboards

--- json2csv.py
+++ json2csv.py
@@ -6,7 +6,7 @@ import sys
 import os
 import json
 import pandas as pd
-from pandas.io.json import json_normaliz
+from pandas.io.json import json_normalize
 
 pd.options.display.width = 200
 pd.options.display.max_colwidth = 75
@@ -14,19 +14,25 @@ pd.options.display.max_colwidth = 75
 def read_json(filename):
     return json.load(open(filename, 'r'))
 
-pages = read_json('pages.json')
 all_players = []
-
-for i, page in enumerate(pages):
-    if i % 10 == 0:
-        print "{} of {}".format(i+1, len(pages))
-    meta = page['meta']
-    players = page['players']
-    for player in players:
-        player.update(meta)
-        all_players.append(player)
+for leaderboard in ('casual', 'ranked', 'general'):
+    print "*"*80
+    print leaderboard.upper()
+    print "*"*80
+    pages = read_json('{}-success.json'.format(leaderboard))
+    for i, page in enumerate(pages):
+        if i % 10 == 0:
+            print "{} of {}".format(i+1, len(pages))
+        meta = page['meta']
+        players = page['players']
+        for player in players:
+            player.update(meta)
+            all_players.append(player)
 
 df_players = json_normalize(all_players)
-df_players.to_csv('players.csv', sep=',', encoding='utf-8', index=False)
+outfile = 'players.csv'
+df_players.to_csv(outfile, sep=',', encoding='utf-8', index=False)
+
+print "Saved {} rows to {}".format(len(df_players), outfile)
 
 

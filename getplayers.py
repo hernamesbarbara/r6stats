8d07c279010c757b3f0d11f2e4b548df3f42d552
austin <austinogilvie@gmail.com>
2017-03-19 15:39:38 -0400

    players

--- getplayers.py
+++ getplayers.py
@@ -1,68 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
-"""getplayers.py
+"""playerapi.py
 """
 import sys
 import os
 import json
 import requests
-import collections
+import pandas as pd
 
-def get_url(leaderboard):
+
+def get_url(platform, username):
     base = "https://api.r6stats.com"
     version = '/api/v1'
-    endpoint =  '/leaderboards/{}'.format(leaderboard)
+    endpoint = '/players/{username}/?platform={platform}'.format(platform=platform, username=username)
     return "{base}{version}{endpoint}".format(base=base, version=version, endpoint=endpoint)
 
-def get_page(url, page=1):
-    return requests.get(url, params={'page': page})
 
-def write_json_to_file(data, outfile):
-    print "writing {}".format(outfile)
-    with open(outfile, "w") as o:
-        json.dump(pages, o, encoding='utf8', indent=2)
-    return
+df = pd.read_csv('data/players.csv')
 
-def write_text_to_file(data, outfile):
-    print "writing {}".format(outfile)
-    with open(outfile, "w") as e:
-        e.write(os.linesep.join(data))
-    return
-
-def get_all_pages(leaderboard):
-    pages = []
-    errors = []
-    url = get_url(leaderboard)
-    r = get_page(url)
-    page = r.json()
-    pages.append(page)
-    meta = page['meta']
-    next_page = meta['next_page']
-    total_pages = meta['total_pages']
-    for i in xrange(next_page, total_pages+1):
-        if i % 10 == 0 or i == total_pages:
-            print "{} => {} of {}".format(leaderboard, i, total_pages)
-        try:
-            r = get_page(url, page=i)
-            page = r.json()
-            page['meta'][u'url'] = r.url
-            pages.append(page)
-        except Exception, err:
-            r = requests.Request('GET', url, params={'page':i}).prepare()
-            page = None
-            errors.append(r.url)
-    return pages, errors
-
-
-for leaderboard in ('ranked', 'general'):
-    print "*"*80
-    print leaderboard.upper()
-    print "*"*80
-    pages, errors = get_all_pages(leaderboard)
-
-    success_file = "data/{}-success.json".format(leaderboard)
-    write_json_to_file(pages, success_file)
-
-    error_file = "data/{}-errors.txt".format(leaderboard)
-    write_text_to_file(errors, error_file)
-        
+# for player in players:
+#     get the data goes here..
+#     url = get_url(platform, username)
+#     r = requests.get(url)
+#     data = r.json()

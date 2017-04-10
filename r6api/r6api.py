c2ecf5717b776ca5a5455fedf1d6078e9e7cf63e
austin <austinogilvie@gmail.com>
2017-04-10 16:15:31 -0400

    r6api

--- r6api/r6api.py
+++ r6api/r6api.py
@@ -1,40 +1,74 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 """r6api.py
 """
 import sys
 import os
 import json
 import requests
-import hammock
 import copy
 
 
+class DotDict(dict):
+    """
+    dict that supports both dot.notation and sub['notation']
+    """
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
-class R6Api(hammock.Hammock):
+    def __init__(self, dct):
+        for key, value in dct.items():
+            if hasattr(value, 'keys'):
+                value = DotDict(value)
+            self[key] = value
 
-    ENDPOINTS = ['leaderboards']
+
+class Endpoint(object):
+    def __init__(self, base_url, child_endpoints):
+        self._base_url = base_url
+        self._endpoints = child_endpoints
+        for endpoint, children in child_endpoints.items():
+            if endpoint=='methods':
+                for method, function in children.items():
+                    def wrapper(params={}):
+                        return function(self._base_url, params)
+                    setattr(self, method, wrapper)
+                continue
+            setattr(self, endpoint, Endpoint(os.path.join(base_url, endpoint), children))
+
+    def __str__(self):
+        return self._base_url
+
+    def __repr__(self):
+        return "{}({})".format(self.__class__.__name__, self._base_url)
+
+
+def GET(url, params={}):
+    data = requests.get(url, params).json()
+    return DotDict(data)
+
+
+class R6Api(Endpoint):
+    BASE_URL = 'https://api.r6stats.com/api/v1'
+    ENDPOINTS = {
+        'leaderboards': {
+            'casual': {'methods': {'GET': GET}},
+            'general': {'methods': {'GET': GET}},
+            'ranked': {'methods': {'GET': GET}}
+        }
+    }
     
-    def __init__(self, name='https://api.r6stats.com/api/v1', parent=None, **kwargs):
-        super(R6Api, self).__init__(name, parent, **kwargs)
-    
-    def _url(self, *args):
-        return super(R6Api, self)._url(*args)
+    def __init__(self):
+        super(R6Api, self).__init__(BASE_URL, ENDPOINTS)
 
-r6 = R6Api()
-seen = []
+    def __str__(self):
+        return self._base_url
 
-STOP_AFTER = 3
+    def __repr__(self):
+        return "{}({})".format(self.__class__.__name__, self._base_url)
 
-if len(seen) == 0:
-    page = r6.leaderboards.casual.GET(params={'page':1}).json()
-    total_pages = page['meta']['total_pages']
-    for i in xrange(page['meta']['next_page'], total_pages):
-        try:
-            page = r6.leaderboards.casual.GET(params={'page': i}).json()
-            seen.append(page)
-        except:
-            continue
-        
-        if i >= STOP_AFTER: 
-            break
+
+# api = Endpoint()
+api = R6Api()
+

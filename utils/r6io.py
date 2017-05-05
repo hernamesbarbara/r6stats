f9b953a2bee4ad7e593b7cae207367dc81c77c5a
austin <austinogilvie@gmail.com>
2017-05-04 22:01:10 -0400

    notebook stuff

--- utils/r6io.py
+++ utils/r6io.py
@@ -12,10 +12,8 @@ try:
 except ImportError:
     import json
 
-
 DATE_COLUMNS = ['indexed_at', 'updated_at']
-
-ID_COLUMNS = ['platform', 'ubisoft_id', 'username']
+DIMENSION_COLUMNS = ['platform', 'ubisoft_id', 'username', 'stats.ranked.has_played', 'stats.casual.has_played']
 
 def read_jsonl_stream(stream):
     for line in stream:
@@ -41,35 +39,52 @@ def read_nested_write_flat(infile, outfile):
 def jsonl_to_csv(infile_jsonl, outfile_csv):
     pd.read_json(infile_jsonl, lines=True).to_csv(outfile_csv, index=False, encoding='utf-8')
     print "saved records to {}".format(outfile_csv)
-
-
-def _get_row_mask_for_platform(frame, platform):
-    return frame['platform'].astype(str).fillna('NONE') == platform
-
-def _get_col_mask_for_game_mode_stats(frame, game_mode=""):
-    mask1 = frame.columns.str.startswith('stats.{}'.format(game_mode))
-    mask2 = frame.columns.str.startswith('stats.overall')
-    mask3 = frame.columns.str.endswith('has_played')==False
-    return frame.columns[ (mask1|mask2) & (mask3)]
     
 def select_rows_by_platform(frame, platform):
-    return frame[_get_row_mask_for_platform(frame, platform)]
+    mask = frame['platform'].astype(str).fillna('NONE') == platform
+    return frame[mask]
 
-def select_stats_columns_by_game_mode(frame, game_mode=""):
-    frame = frame.ix[:,_get_col_mask_for_game_mode_stats(frame, game_mode)]
-    return frame.astype(np.float64)
+def select_stats_columns(frame):
+    mask1 = frame.columns.str.startswith('stats.')
+    mask2 = frame.columns.str.endswith('has_played')==False
+    stats_cols = frame.columns[(mask1&mask2)]
+    frame = frame.ix[:, stats_cols]
+    return frame.astype(np.float64).fillna(0)
 
-def read_player_csv(filename_or_dataframe, platform, game_mode):
+def has_playtime(frame, game_mode):
+    mask1 = frame['stats.{}.playtime'.format(game_mode)].fillna(0)>0
+    mask2 = frame['stats.{}.has_played'.format(game_mode)]==True
+    return frame[(mask1&mask2)]
 
+def read_player_csv(filename_or_dataframe, platform, game_mode=None):
     if isinstance(filename_or_dataframe, basestring):
-        df = pd.read_csv(filename_or_dataframe)
+        frame = pd.read_csv(filename_or_dataframe)
     else:
-        df = filename_or_dataframe
-    df_platform = select_rows_by_platform(df, platform)
-    df_id_vars = df_platform[ID_COLUMNS].copy()
+        frame = filename_or_dataframe
+    df_platform = select_rows_by_platform(frame, platform)
+    df_id_vars = df_platform[DIMENSION_COLUMNS].copy()
     df_dates = df_platform[DATE_COLUMNS].copy()
     df_dates.loc[:, DATE_COLUMNS] = df_dates[DATE_COLUMNS].copy().apply(pd.to_datetime)
-    df_stats = select_stats_columns_by_game_mode(df_platform, game_mode)
-    return pd.concat([df_dates, df_id_vars, df_stats], axis=1)
+    df_stats = select_stats_columns(df_platform)
+    df = pd.concat([df_dates, df_id_vars, df_stats], axis=1)
+    df['updated_at'] = df[DATE_COLUMNS].max(axis=1)
+    df = df.drop(['indexed_at'], axis=1)
+    df = df.sort_values(by=['username', 'updated_at'], ascending=[True, False])
+    df = df.drop_duplicates(['username'])
+    bools = df.columns[df.columns.str.endswith('has_played')]
+    df[bools] = df[bools].astype(np.bool)
+    if game_mode:
+        if game_mode == 'ranked':
+            # keep only ranked players with playtime. drop all casual columns.
+            df = has_playtime(df, 'ranked')
+            df = df.drop(df.columns[df.columns.str.contains("casual")], axis=1)
+        else:
+            # keep only casual players with playtime. drop all ranked columns.
+            df = has_playtime(df, 'casual')
+            df = df.drop(df.columns[df.columns.str.contains("ranked")], axis=1)
+    return df
+
+
+
 
 

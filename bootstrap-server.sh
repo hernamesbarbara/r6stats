763ffd9e7b2dd2d9575ab84452ecc7bbb84c219d
austin <austinogilvie@gmail.com>
2017-11-21 09:48:25 -0500

    bootstrap bash_aliases

--- bootstrap-server.sh
+++ bootstrap-server.sh
@@ -5,3 +5,6 @@ sudo apt-get update
 sudo apt install python3-pip
 pip3 install --upgrade pip
 sudo apt install ipython3
+
+
+curl https://raw.githubusercontent.com/hernamesbarbara/dotfiles/master/bash_aliases > ~/.bash_aliases

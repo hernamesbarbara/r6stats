b6584db47b88a19b79b2384897681e90f11e806a
austin <austinogilvie@gmail.com>
2017-11-23 17:56:25 -0500

    json2csv improved

--- scripts/json2csv.py
+++ scripts/json2csv.py
@@ -41,16 +41,42 @@ def dump_jsonl_stream(data, stream):
     except:
         pass
 
-def read_nested_write_flat(infile, outfile):
+def flatten_page(page):
+    meta = page['meta']
+    players = page['players']
+    for player in players:
+        try:
+            res = nested_to_record(player)
+            for k, v in meta.items():
+                res['meta.{}'.format(k)] = v
+            yield res
+        except Exception as err:
+            sys.stderr.write(str(err))
+            sys.stderr.flush()
+            continue
+
+def jsonl_to_csv(infile, outfile):
+    "read line delim json file. flatten each nested record. save data to csv."
     with open(infile, 'r') as infile:
         with open(outfile, 'a') as outfile:
-            for record in read_jsonl_stream(infile):
-                dump_jsonl_stream(nested_to_record(record), outfile)
+            for i, page in enumerate(read_jsonl_stream(infile)):
+                    try:
+                        page = flatten_page(page)
+                        if i == 0:
+                            header = True
+                        else:
+                            header = False
+                        pd.DataFrame(page).to_csv(
+                            outfile, 
+                            header=header,
+                            index=False,
+                            encoding='utf-8'
+                        )
 
-def jsonl_to_csv(infile_jsonl, outfile_csv):
-    pd.read_json(infile_jsonl, lines=True)\
-        .to_csv(outfile_csv, index=False, encoding='utf-8')
-    print("saved records to {}".format(outfile_csv))
+                    except Exception as err:
+                        sys.stderr.write(str(err))
+                        sys.stderr.flush()
+                        continue
 
 def _parse_args():
     args = docopt.docopt(__doc__, argv=" ".join(sys.argv[1:]), 
@@ -61,27 +87,19 @@ def _parse_args():
         f_name, _ = os.path.splitext(infile)
         o_jsonl = "{}-flat.jsonl".format(f_name)
         o_csv = "{}.csv".format(f_name)
-        return (infile, dirname, f_name, o_jsonl, o_csv)
+        return (infile, dirname, f_name, o_csv)
     except Exception as err:
         sys.stderr.write(str(err)+os.linesep)
         sys.stderr.write(__doc__)
         sys.exit(1)
 
 def main():
-    (infile, dirname, f_name, o_jsonl, o_csv) = _parse_args()
-    if os.path.exists(o_jsonl):
-        os.remove(o_jsonl)
-    # next line
-    # CPU times: user 5min 59s, sys: 2.22 s, total: 6min 2s
-    # Wall time: 6min 3s
-    print("Flattening json...")
-    read_nested_write_flat(infile, o_jsonl)
-    
+    (infile, dirname, f_name, o_csv) = _parse_args()
     # next line
     # CPU times: user 1min 14s, sys: 7.69 s, total: 1min 22s
     # Wall time: 1min 23s
     print("Saving to csv...")
-    jsonl_to_csv(o_jsonl, o_csv)
+    jsonl_to_csv(infile, o_csv)
 
 if __name__ == '__main__':
     main()
